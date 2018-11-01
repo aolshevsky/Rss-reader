6ccd8ae3de5be9bbe9e486f5c1adaeff7af07cdd
po4emy4ka <aleksey_olshevski@mail.ru>
2018-11-01 19:15:30 +0300

    Add base functional to permissions

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,13 +1,151 @@
 package com.example.user.myapplication;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.File;
+
+import static android.Manifest.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String ver_name = "app version:" + BuildConfig.VERSION_NAME;
+        Button btn = (Button) findViewById(R.id.start_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (hasPermissions()){
+                    // our app has permissions.
+                    makeFolder();
+
+                }
+                else {
+                    //our app doesn't have permissions, So i m requesting permissions.
+                    requestPerms();
+                }
+            }
+        });
     }
+
+    private void makeFolder(){
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(),"fandroid");
+
+        if (!file.exists()){
+            Boolean ff = file.mkdir();
+            if (ff){
+                Toast.makeText(MainActivity.this, "Folder created successfully", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Failed to create folder", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else {
+            Toast.makeText(MainActivity.this, "Folder already exist", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean hasPermissions(){
+        int res = 0;
+        //string array of permissions,
+        String[] permissions = new String[]{permission.WRITE_EXTERNAL_STORAGE};
+
+        for (String perms : permissions){
+            res = checkCallingOrSelfPermission(perms);
+            if (!(res == PackageManager.PERMISSION_GRANTED)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void requestPerms(){
+        String[] permissions = new String[]{permission.WRITE_EXTERNAL_STORAGE};
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permissions,PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        boolean allowed = true;
+
+        switch (requestCode){
+            case PERMISSION_REQUEST_CODE:
+
+                for (int res : grantResults){
+                    // if user granted all permissions.
+                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
+                }
+
+                break;
+            default:
+                // if user not granted permissions.
+                allowed = false;
+                break;
+        }
+
+        if (allowed){
+            //user granted all permissions we can perform our task.
+            makeFolder();
+        }
+        else {
+            // we will give warning to user that they haven't granted permissions.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(permission.WRITE_EXTERNAL_STORAGE)){
+                    Toast.makeText(this, "Storage Permissions denied.", Toast.LENGTH_SHORT).show();
+                    requestPerms();
+                } else {
+                    showNoStoragePermissionSnackbar();
+                }
+            }
+        }
+
+    }
+
+
+
+    public void showNoStoragePermissionSnackbar() {
+        Snackbar.make(MainActivity.this.findViewById(R.id.activity_view), "Storage permission isn't granted" , Snackbar.LENGTH_LONG)
+                .setAction("SETTINGS", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openApplicationSettings();
+
+                        Toast.makeText(getApplicationContext(),
+                                "Open Permissions and grant the Storage permission",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                })
+                .show();
+    }
+
+    public void openApplicationSettings() {
+        Intent appSettingsIntent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(appSettingsIntent, PERMISSION_REQUEST_CODE);
+    }
+
+
 }
