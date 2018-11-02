f10b4225ad955974e1c6892b93a2486dd0416989
po4emy4ka <aleksey_olshevski@mail.ru>
2018-11-02 13:11:20 +0300

    Add keystoreProperties

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
@@ -28,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String ver_name = "app version:" + BuildConfig.VERSION_NAME;
+
+        TextView ver_view = findViewById(R.id.version_name);
+        String ver_name = "app version: " + BuildConfig.VERSION_NAME;
+        ver_view.setText(ver_name);
         Button btn = (Button) findViewById(R.id.start_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
