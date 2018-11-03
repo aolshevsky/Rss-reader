563353c54fa4d5a34613e7fb3f8a3d961c8d744d
po4emy4ka <aleksey_olshevski@mail.ru>
2018-11-03 20:36:34 +0300

    Add resources. Fixed gradle and checking permissions

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.user.myapplication;
 
-import android.annotation.SuppressLint;
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -12,10 +12,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static android.Manifest.permission;
 
@@ -23,14 +27,19 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_REQUEST_CODE = 123;
 
+    final String LOG_TAG = "myLogs";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d(LOG_TAG, "onCreate");
+
         TextView ver_view = findViewById(R.id.version_name);
-        String ver_name = "app version: " + BuildConfig.VERSION_NAME;
+        String ver_name = getResources().getString(R.string.app_ver) + BuildConfig.VERSION_NAME;
         ver_view.setText(ver_name);
+        showPhoneState();
         Button btn = (Button) findViewById(R.id.start_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -38,14 +47,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (hasNeedPermissions()) {
                     showPhoneState();
-                } else {
+                }
+                else {
                     requestPerms();
                 }
             }
         });
     }
 
-    @SuppressLint("HardwareIds")
     private void showPhoneState() {
         TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         String imei = "IMEI: ";
@@ -54,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
             imei += tm.getDeviceId();
             imei_txt.setText(imei);
         } catch (SecurityException e) {
-            imei_txt.setText(e.getMessage());
+            imei_txt.setText(getResources().getString(R.string.msg_no_per));
         }
     }
 
@@ -81,33 +90,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        boolean allowed = true;
 
-        switch (requestCode){
-            case PERMISSION_REQUEST_CODE:
-
-                for (int res : grantResults){
-                    // if user granted all permissions.
-                    allowed = allowed && (res == PackageManager.PERMISSION_GRANTED);
+        Map<String, Boolean> allowed = new HashMap<>();
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: {
+                for (int i = 0, len = grantResults.length; i < len; i++) {
+                    allowed.put(permissions[i] ,(grantResults[i] == PackageManager.PERMISSION_GRANTED));
                 }
-
                 break;
-            default:
-                // if user not granted permissions.
-                allowed = false;
-                break;
+            }
         }
-
-        if (allowed){
-            //user granted all permissions we can perform our task.
+        if (allowed.get(permission.READ_PHONE_STATE)){
             showPhoneState();
         }
         else {
             // we will give warning to user that they haven't granted permissions.
             if (shouldShowRequestPermissionRationale(permission.READ_PHONE_STATE)) {
-                Toast.makeText(this, "Phone Permissions denied.", Toast.LENGTH_SHORT).show();
                 requestPerms();
-            } else {
+                Toast.makeText(this, getResources().getString(R.string.msg_ph_per_den), Toast.LENGTH_SHORT).show();
+            }
+            else {
                 showNoPhonePermissionSnackbar();
             }
         }
@@ -116,14 +118,14 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void showNoPhonePermissionSnackbar() {
-        Snackbar.make(MainActivity.this.findViewById(R.id.activity_view), "Phone permission isn't granted" , Snackbar.LENGTH_LONG)
+        Snackbar.make(MainActivity.this.findViewById(R.id.activity_view), getResources().getString(R.string.msg_ph_per_no_grtd) , Snackbar.LENGTH_LONG)
                 .setAction("SETTINGS", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         openApplicationSettings();
 
                         Toast.makeText(getApplicationContext(),
-                                "Open Permissions and grant the Phone permission",
+                                getResources().getString(R.string.msg_open_per),
                                 Toast.LENGTH_SHORT)
                                 .show();
                     }
