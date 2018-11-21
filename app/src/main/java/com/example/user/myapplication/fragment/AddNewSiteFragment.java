b69866bbe2465e5c2d14666323fc542cb8384720
po4emy4ka <campapp.games@gmail.com>
2018-11-21 22:06:02 +0300

    Add load rss feed

--- app/src/main/java/com/example/user/myapplication/fragment/AddNewSiteFragment.java
+++ app/src/main/java/com/example/user/myapplication/fragment/AddNewSiteFragment.java
@@ -1,6 +1,9 @@
 package com.example.user.myapplication.fragment;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +13,10 @@ import android.widget.Toast;
 
 import com.example.user.myapplication.R;
 import com.example.user.myapplication.activity.MainActivity;
+import com.example.user.myapplication.model.RSSFeed;
+import com.example.user.myapplication.model.WebSiteModel;
 import com.example.user.myapplication.utils.Parser;
+import com.example.user.myapplication.utils.RSSParser;
 
 import androidx.fragment.app.Fragment;
 import es.dmoral.toasty.Toasty;
@@ -21,6 +27,9 @@ public class AddNewSiteFragment extends Fragment {
     private View view;
     private EditText editTextUrl;
 
+    private ProgressDialog pDialog;
+
+    private RSSFeed rssFeed;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,8 +53,9 @@ public class AddNewSiteFragment extends Fragment {
 
                 int valid_val = Parser.ValidUrl(url);
 
+                valid_val = 0;
                 if(valid_val == 0){
-                    //new loadRSSFeed().execute(url);
+                    new loadRSSFeed().execute(url);
                 } else if(valid_val == 1){
                     onErrorMessage("Please enter a valid url");
                 } else {
@@ -65,8 +75,48 @@ public class AddNewSiteFragment extends Fragment {
     }
 
 
-    public void onErrorMessage(String message) {
+    private void onErrorMessage(String message) {
         Toasty.error(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    class loadRSSFeed extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(getContext());
+            pDialog.setMessage("Fetching RSS Information ...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+
+        @Override
+        protected String doInBackground(String... args) {
+            String url = args[0];
+            url = "https://news.tut.by/rss/index.rss";
+            rssFeed = RSSParser.getInstance().getRSSFeed(url);
+            Log.d("rssFeed", " "+ rssFeed);
+            if (rssFeed != null) {
+                Log.d("url",
+                        "Pid" + rssFeed.getTitle() + " " + rssFeed.getLink() + " "
+                                + rssFeed.getDescription() + " "
+                                + rssFeed.getLanguage());
+                ////
+                WebSiteModel site = new WebSiteModel(rssFeed.getTitle(), rssFeed.getLink(), rssFeed.getRSSLink(),
+                        rssFeed.getDescription());
+            } else {
+                onErrorMessage("Rss url not found. Please check the url or try again");
+            }
+            return null;
+        }
+
+
+        protected void onPostExecute(String args) {
+            pDialog.dismiss();
+        }
+
     }
 
 }
